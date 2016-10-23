f58e08c4a83cb7d2405312281f2a0a5747a7c4d6
goldjay <jay.steingold@gmail.com>
2016-10-23 14:13:16 +0900

    Finished project5

--- project5/project5/ViewController.swift
+++ project5/project5/ViewController.swift
@@ -17,8 +17,13 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //UIAlertController with space for the user to make an answer
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        //Loads words from start.text into the allWords array
         if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
             if let startWords = try? String(contentsOfFile: startWordsPath) {
+                //Separator to divide words
                 allWords = startWords.components(separatedBy: "\n")
             }
         } else{
@@ -27,13 +32,114 @@ class ViewController: UITableViewController {
         startGame()
     }
     
+    func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        //Adds editable text input field to UIAlertController
+        ac.addTextField()
+        
+        //If expecting a closure, you can pass final parameter in braces instaed
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] _ in
+                //"Unowned" means you do not want strong references for those variables
+                    //Meaning the variables may be destroyed
+                //Alert controller and our current viewcontroller are to be captured as unowned references inside the closure
+                //Everything before "in" describes the closure
+                //Everything after "in" is the closure
+            
+            let answer = ac.textFields![0] //Pulls answer from text field
+            self.submit(answer: answer.text!)
+        }
+        
+        //Adds an action to the UIAlertController
+        ac.addAction(submitAction)
+        
+        present(ac, animated: true)
+    }
+    
+    func submit(answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        let errorTitle: String
+        let errorMessage: String
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    //Insert a row into the table view instead of using reloadData()
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    return
+                }
+                else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
+                }
+            }
+            else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
+            }
+        }
+        else {
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from '\(title!.lowercased())'!"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = title!.lowercased()
+        
+        for letter in word.characters {
+            //If the letter is found in the word, remove it
+            if let pos = tempWord.range(of: String(letter)) {
+                tempWord.remove(at: pos.lowerBound)
+            }
+            else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        //iOS class designed to spot spelling errors
+        let checker = UITextChecker()
+        //Use utf16 when working with UIKit, SpriteKit or any apple framework
+        let range = NSMakeRange(0, word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        //If not mistakes are found, it returns NSNotFound, otherwise returns the misspelling location
+        return misspelledRange.location == NSNotFound
+    }
+    
     func startGame() {
         allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
             title = allWords[0]
+        //Clears the words we used in the previous game, if any
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
+    
+    //Counts the number of used words
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
