03ca6a9fcdbabd7ee440aad9d5f817216df920e9
goldjay <jay.steingold@gmail.com>
2016-10-25 21:44:46 +0900

    First commit project7

---
+++ project7/project7/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  project7
+//
+//  Created by Jay Steingold on 10/25/16.
+//  Copyright © 2016 Jay Steingold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
