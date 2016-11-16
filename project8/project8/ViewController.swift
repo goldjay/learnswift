55829925a5be3a84da9151ec37b6afbbb0b798b6
goldjay <jay.steingold@gmail.com>
2016-11-16 21:20:24 +0900

    initial commit

---
+++ project8/project8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project8
+//
+//  Created by Jay Steingold on 11/16/16.
+//  Copyright © 2016 Jay Steingold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
