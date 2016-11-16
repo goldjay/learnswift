52a526e656f65ee7e5b1a2b9834a9a3150ab01e3
goldjay <jay.steingold@gmail.com>
2016-11-16 22:24:49 +0900

    Added level assets

--- project8/project8/ViewController.swift
+++ project8/project8/ViewController.swift
@@ -14,13 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var answersLabel: UILabel!
     @IBOutlet weak var currentAnswer: UITextField!
     @IBOutlet weak var scoreLabel: UILabel!
+    
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solution = [String]()
+    
+    var score = 0
+    var level = 1
+    
     @IBAction func submitTapped(_ sender: AnyObject) {
     }
     @IBAction func clearTapped(_ sender: AnyObject) {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Loops through all views inside viewController
+        //Modify only if the tag is 1001
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
+    }
+    
+    func letterTapped(btn: UIButton) {
+        
     }
 
     override func didReceiveMemoryWarning() {
