900bde9e0c72b09fcd0b4df1cde74995636f155f
goldjay <jay.steingold@gmail.com>
2016-11-26 20:41:29 +0900

    Added custom cell subclass

---
+++ project10/project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Jay Steingold on 11/26/16.
+//  Copyright © 2016 Jay Steingold. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
--- project10/project10/ViewController.swift
+++ project10/project10/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //How many items you want to show in the grid
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+        return cell
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
