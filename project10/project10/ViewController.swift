4f14740f157a9aca633492edd76a4d3f497a7852
goldjay <jay.steingold@gmail.com>
2016-11-26 20:13:17 +0900

    Initial commit. Added collection view

---
+++ project10/project10/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project10
+//
+//  Created by Jay Steingold on 11/26/16.
+//  Copyright © 2016 Jay Steingold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
